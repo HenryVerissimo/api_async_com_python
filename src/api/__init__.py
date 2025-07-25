633a7a9a13c5e0251d960a4e5748d0720b857dbb
Henrique Verissimo <henriquedevsp@gmail.com>
2025-07-25 16:06:20 -0300

    chore(doc) create and update docstrings
    -add new docstrings in conectiondatabase class
    -add new docstrings in User model class
    -add new docstrings in create_app function
    -add some __init__.py files

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,6 +3,8 @@ from typing import Optional
 
 
 def create_app(config_class: Optional[object] = None) -> Quart:
+    """Create and return a application instance"""
+
     app = Quart(__name__)
     if config_class:
         app.config.from_object(config_class)
--- src/database/mysql/configs/database_connection.py
+++ src/database/mysql/configs/database_connection.py
@@ -1,35 +1,48 @@
 from sqlmodel import create_engine, Session
 from sqlalchemy.engine import Engine
+from typing import Self
 from decouple import config
 
 
 class MySQLConnectionHandler:
+    """
+    Create and manipulate the connection to the MySQL database.
+    """
+
     _engine: Engine | None = None
 
     def __init__(self):
         self._database_uri: str = config("DATABASE_URI", cast=str)
         self.session: Session | None = None
 
-    def connect_db(self):
-        """Create session with database"""
+    def connect_db(self) -> None:
+        """Create the session with the database"""
 
         self.session = Session(self.engine)
 
-    def close_connection(self):
+    def close_connection(self) -> None:
+        """Close the session with the database"""
+
         if self.session:
             self.session.close()
             self.session = None
 
-    def __enter__(self):
+    def __enter__(self) -> Self:
         self.connect_db()
 
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.close_connection()
 
     @property
     def engine(self) -> Engine:
+        """
+        Returns a singleton Engine instance.
+
+        Ensures only Engine is created and reused for all database sessions.
+        """
+
         if not MySQLConnectionHandler._engine:
             MySQLConnectionHandler._engine = create_engine(self._database_uri)
 
