c81f1c081d474dbc8c90b1a14cf43971c87b1ce0
Henrique Verissimo <henriquedevsp@gmail.com>
2025-07-27 17:59:14 -0300

    feat(Quart): register users blueprint
    - register new blueprint to user routes

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,8 @@
 from quart import Quart
 from typing import Optional
 
+from .routes import users_bp
+
 
 def create_app(config_class: Optional[object] = None) -> Quart:
     """Create and return a application instance"""
@@ -8,5 +10,7 @@ def create_app(config_class: Optional[object] = None) -> Quart:
     app = Quart(__name__)
     if config_class:
         app.config.from_object(config_class)
+
+    app.register_blueprint(users_bp, url_prefix="/users")
 
     return app
